a6ef425773188a09320eeaea4ec3bf2b3140923d
Mattias <mattias.bosveld@windesheim.nl>
2024-12-06 12:06:15 +0100

    edit table name in mob entries

--- mob_survival/MOB_SETTINGS.lua
+++ mob_survival/MOB_SETTINGS.lua
@@ -4,14 +4,14 @@
 --! if you want to modify any of these settings just edit the file in <YOUR_WORLD_FOLDER>/mob_survival/
 --Add mobs with a given difficulty
 mob_survival.mobdiffs = {}
-mobdiffs["forgotten_monsters:meselord"] = 20
-mobdiffs["forgotten_monsters:golem"] = 40
-mobdiffs["forgotten_monsters:sking"] = 60
-mobdiffs["forgotten_monsters:growler"] = 3
-mobdiffs["forgotten_monsters:ssword"] = 2
-mobdiffs["forgotten_monsters:skull_berserker"] = 2
-mobdiffs["forgotten_monsters:skull"] = 1
-mobdiffs["forgotten_monsters:sarchers"] = 1
+mob_survival.mobdiffs["forgotten_monsters:meselord"] = 20
+mob_survival.mobdiffs["forgotten_monsters:golem"] = 40
+mob_survival.mobdiffs["forgotten_monsters:sking"] = 60
+mob_survival.mobdiffs["forgotten_monsters:growler"] = 3
+mob_survival.mobdiffs["forgotten_monsters:ssword"] = 2
+mob_survival.mobdiffs["forgotten_monsters:skull_berserker"] = 2
+mob_survival.mobdiffs["forgotten_monsters:skull"] = 1
+mob_survival.mobdiffs["forgotten_monsters:sarchers"] = 1
 
 --Add the total difficulty of all mobs that can spawn in a wave.
 --The difficulty for each wave is total_mob_diff*wave_number.

6fd7dded7e15ae61a18176a4e76506be59dc5cf4
Mattias <mattias.bosveld@windesheim.nl>
2024-12-10 05:41:57 +0100

    add print pointed_thing

--- rangedweapons/aa12.lua
+++ rangedweapons/aa12.lua
@@ -62,9 +62,10 @@ minetest.register_tool("rangedweapons:aa12_rrr", {
 		gun_sound = "rangedweapons_shotgun_shot",
 	},
 	on_secondary_use = function(itemstack, user, pointed_thing)
-rangedweapons_reload_gun(itemstack, user)
-return itemstack
-end,
+		print(pointed_thing)
+		rangedweapons_reload_gun(itemstack, user)
+		return itemstack
+	end,
 })
 
 

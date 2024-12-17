8c2b326e366644b100951da60ee16a073c7b5bef
Mattias <mattias.bosveld@windesheim.nl>
2024-12-17 20:07:25 +0100

    add moblist_id comparison now to replace unloaded mob with new luaentity

--- mob_survival/src/arena_manager.lua
+++ mob_survival/src/arena_manager.lua
@@ -78,41 +78,15 @@ local function startswith(string, start)
     return string:sub(1, #start) == start
 end
 
-function tprint (tbl, indent)
-    if not indent then indent = 0 end
-    local toprint = string.rep(" ", indent) .. "{\r\n"
-    indent = indent + 2 
-    for k, v in pairs(tbl) do
-      toprint = toprint .. string.rep(" ", indent)
-      if (type(k) == "number") then
-        toprint = toprint .. "[" .. k .. "] = "
-      elseif (type(k) == "string") then
-        toprint = toprint  .. k ..  "= "   
-      end
-      if (type(v) == "number") then
-        toprint = toprint .. v .. ",\r\n"
-      elseif (type(v) == "string") then
-        toprint = toprint .. "\"" .. v .. "\",\r\n"
-      elseif (type(v) == "table") then
-        toprint = toprint .. tprint(v, indent + 2) .. ",\r\n"
-      else
-        toprint = toprint .. "\"" .. tostring(v) .. "\",\r\n"
-      end
-    end
-    toprint = toprint .. string.rep(" ", indent-2) .. "}"
-    return toprint
-  end
-
 function on_time_tick(arena)
     for _, entity in pairs(minetest.luaentities) do
-        print(tprint(entity))
         if startswith(entity.name, "mobs_mc:") then
             local nametag = entity.object:get_nametag_attributes()
             if nametag then
                 if nametag.text == "" then
                     -- Refresh entityref if mob is unloaded
                     for i, mob in pairs(mob_survival.moblist) do
-                        if 1 == 0 then
+                        if mob.moblist_id == entity.moblist_id then
                             print("entity replaced")
                             mob_survival.moblist[i] = entity
                         end

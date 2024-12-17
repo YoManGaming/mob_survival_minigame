4c702e8e70ccca75b2afb9c3765037bdbe4ba4ff
Mattias <mattias.bosveld@windesheim.nl>
2024-12-17 16:47:23 +0100

    move autoqueue to below minigame registration

--- mob_survival/init.lua
+++ mob_survival/init.lua
@@ -19,24 +19,6 @@ minetest.register_chatcommand("setspawn", {
   end,
 })
 
--- Autoqueue for arena
-minetest.register_on_joinplayer(function(player)
-  local name = player:get_player_name()
-  minetest.chat_send_player(name, "Welcome to the minigame Mob Survival! You have now been queued for the next game. "..
-  "If you want to leave the queue and go back to the lobby, use the command /leave")
-  arena_lib.join_queue("mob_survival", "mob_arena", name)
-end)
-
-minetest.register_chatcommand("leave", {
-  description = "Leave",
-  func = function(name)
-    if arena_lib.is_player_playing(name, "mob_survival") then
-      arena_lib.remove_player_from_arena(name, 3, "Server")
-      -- TODO: Send to lobby server
-    end
-  end
-})
-
 arena_lib.register_minigame("mob_survival", {
     name = "Mob Survival",
     prefix = "[Mob Survival] ",
@@ -58,6 +40,24 @@ arena_lib.register_minigame("mob_survival", {
       
     }
 )
+
+-- Autoqueue for arena
+minetest.register_on_joinplayer(function(player)
+  local name = player:get_player_name()
+  minetest.chat_send_player(name, "Welcome to the minigame Mob Survival! You have now been queued for the next game. "..
+  "If you want to leave the queue and go back to the lobby, use the command /leave")
+  arena_lib.join_queue("mob_survival", "mob_arena", name)
+end)
+
+minetest.register_chatcommand("leave", {
+  description = "Leave",
+  func = function(name)
+    if arena_lib.is_player_playing(name, "mob_survival") then
+      arena_lib.remove_player_from_arena(name, 3, "Server")
+      -- TODO: Send to lobby server
+    end
+  end
+})
 
 mob_survival.path = minetest.get_modpath(minetest.get_current_modname())
 

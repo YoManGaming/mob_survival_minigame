fdfdbed09b1eb1e6b49034a941d3078e3ed4faf0
Mattias <mattias.bosveld@windesheim.nl>
2024-12-18 19:41:28 +0100

    start unit testing api

--- mob_survival/init.lua
+++ mob_survival/init.lua
@@ -90,3 +90,31 @@ dofile(mob_survival.path.."/src/arena_manager.lua")
 dofile(mob_survival.path.."/src/api.lua")
 dofile(mob_survival.path.."/src/shop.lua")
 dofile(mob_survival.path.."/src/shopkeeper.lua")
+
+--unit test
+function tprint (tbl, indent)
+  if not indent then indent = 0 end
+  local toprint = string.rep(" ", indent) .. "{\r\n"
+  indent = indent + 2 
+  for k, v in pairs(tbl) do
+    toprint = toprint .. string.rep(" ", indent)
+    if (type(k) == "number") then
+      toprint = toprint .. "[" .. k .. "] = "
+    elseif (type(k) == "string") then
+      toprint = toprint  .. k ..  "= "   
+    end
+    if (type(v) == "number") then
+      toprint = toprint .. v .. ",\r\n"
+    elseif (type(v) == "string") then
+      toprint = toprint .. "\"" .. v .. "\",\r\n"
+    elseif (type(v) == "table") then
+      toprint = toprint .. tprint(v, indent + 2) .. ",\r\n"
+    else
+      toprint = toprint .. "\"" .. tostring(v) .. "\",\r\n"
+    end
+  end
+  toprint = toprint .. string.rep(" ", indent-2) .. "}"
+  return toprint
+end
+print(tprint(mob_survival.get_leaderboard()))
+mob_survival.register_tracker("mobs_mc:zombie", 10, false)

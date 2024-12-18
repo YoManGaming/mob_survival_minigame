c77c88a8ac80a0cc7fc5e3d2aa5820291be41bbf
Mattias <mattias.bosveld@windesheim.nl>
2024-12-18 20:06:38 +0100

    add table reference correctly in updating progress and unit test get_progress

--- mob_survival/init.lua
+++ mob_survival/init.lua
@@ -92,5 +92,30 @@ dofile(mob_survival.path.."/src/shop.lua")
 dofile(mob_survival.path.."/src/shopkeeper.lua")
 
 --unit test
-print(mob_survival.get_leaderboard())
-mob_survival.register_tracker("mobs_mc:zombie", 10, false)
+function tprint (tbl, indent)
+  if not indent then indent = 0 end
+  local toprint = string.rep(" ", indent) .. "{\r\n"
+  indent = indent + 2 
+  for k, v in pairs(tbl) do
+    toprint = toprint .. string.rep(" ", indent)
+    if (type(k) == "number") then
+      toprint = toprint .. "[" .. k .. "] = "
+    elseif (type(k) == "string") then
+      toprint = toprint  .. k ..  "= "   
+    end
+    if (type(v) == "number") then
+      toprint = toprint .. v .. ",\r\n"
+    elseif (type(v) == "string") then
+      toprint = toprint .. "\"" .. v .. "\",\r\n"
+    elseif (type(v) == "table") then
+      toprint = toprint .. tprint(v, indent + 2) .. ",\r\n"
+    else
+      toprint = toprint .. "\"" .. tostring(v) .. "\",\r\n"
+    end
+  end
+  toprint = toprint .. string.rep(" ", indent-2) .. "}"
+  return toprint
+end
+
+local result = mob_survival.get_progress("mcl_mobs:zombie", "YoManGaming101")
+print(tprint(result))

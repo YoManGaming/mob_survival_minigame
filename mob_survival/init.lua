0ab546f975bd611cb44cd5df664c79b85239feb1
Mattias <mattias.bosveld@windesheim.nl>
2024-12-19 16:55:30 +0100

    edit "mod" to mob_survival

--- mob_survival/init.lua
+++ mob_survival/init.lua
@@ -86,7 +86,7 @@ arena_lib.register_on_leave_queue(function(mod, arena, p_name, has_queue_status_
   --hop_player_to_lobby(p_name)
 end)
 
-arena_lib.on_join_queue(mod, function(arena, p_name)
+arena_lib.on_join_queue("mob_survival", function(arena, p_name)
   if arena.players_amount == 4 then
     arena_lib.force_start(nil, "mob_survival", arena)
   end
